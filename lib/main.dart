3ecd483f7ee45165ca8efa72329b5dd7495421cb
GaoWhitebird <55334868+GaoWhitebird@users.noreply.github.com>
2025-04-02 14:56:13 -0500

    iOS configure

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       builder: (context, child) {
-        return SafeArea(
+        return Container(
           child: child ?? const SizedBox.shrink(),
         );
       },
